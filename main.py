85b50664e3fe1797e34040148ae791d5651b2a1a
Тимур Болгов <timur-bolgov@mail.ru>
2024-01-20 23:17:31 +0300

    Моя дипломная работа по созданию чат бота в телеграм

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import asyncio
+import io
 import logging
-from re import match
+import aiohttp
+from re import Match
 from magic_filter import RegexpMode
 from aiogram import Bot, F
 from aiogram import Dispatcher
 from aiogram import types
-from aiogram.filters import CommandStart
-from aiogram.filters import Command
+from aiogram.filters import CommandStart, Command
 from aiogram.utils import markdown
-from aiogram.enums import ParseMode
+from aiogram.utils.chat_action import ChatActionSender
+from aiogram.enums import ParseMode, ChatAction
 from config import settings
-from config import API_KEY
-from config import BOT_TOKEN
 
 dp = Dispatcher()
+any_media_filter = F.photo | F.video | F.document
 
 
 @dp.message(CommandStart())
@@ -21,19 +22,20 @@ async def handle_start(message: types.Message):
     url = "https://telegrambot.biz/images/avatars/2457.png"
     await message.answer(
         text=f'{markdown.hide_link(url)}Привет {markdown.hbold(message.from_user.full_name)}! Что я могу для тебя сделать?',
-        parse_mode=ParseMode.HTML
+        parse_mode=ParseMode.HTML,
     )
 
 
-@dp.message(Command('help', prefix="/!"))
+@dp.message(Command("help", prefix="!/"))
 async def handle_help(message: types.Message):
-    # text = 'Я простой эхо бот!\nОтправь мне любое сообщение!'
+    # text = "I'm an echo bot.\nSend me any message!"
     # entity_bold = types.MessageEntity(
     #     type="bold",
-    #     offset=len('Я простой эхо бот!\nОтправь мне '),
-    #     length=5)
+    #     offset=len("I'm an echo bot.\nSend me "),
+    #     length=3,
+    # )
     # entities = [entity_bold]
-    #
+    # await message.answer(text=text, entities=entities)
     text = markdown.text(
         markdown.markdown_decoration.quote("Я простой эхо бот."),
         markdown.text(
@@ -61,8 +63,9 @@ async def handle_command_code(message: types.Message):
         "Это язык программирования Python:",
         "",
         markdown.markdown_decoration.pre_language(
+            # markdown.markdown_decoration.pre(
             markdown.text(
-                "print('Hello World!')",
+                "print('Hello world!')",
                 "\n",
                 "def foo():\n    return 'bar'",
                 sep="\n",
@@ -73,51 +76,196 @@ async def handle_command_code(message: types.Message):
         "",
         markdown.markdown_decoration.pre_language(
             markdown.text(
-            "console.log('Hello World!)",
+                "console.log('Hello world!')",
                 "\n",
                 "function foo() {\n  return 'bar'\n}",
-                sep="\n"
+                sep="\n",
             ),
             language="javascript",
         ),
-        sep="\n"
+        sep="\n",
     )
     await message.answer(text=text, parse_mode=ParseMode.MARKDOWN_V2)
 
 
-# @dp.message(is_photo)
-# @dp.message(lambda message: message.photo)
+@dp.message(Command('pic'))
+async def handle_comand_pic(message: types.Message):
+    url = 'https://stihi.ru/pics/2023/03/01/2507.jpg'
+    await message.bot.send_chat_action(
+        chat_id=message.chat.id,
+        action=ChatAction.UPLOAD_PHOTO,
+    )
+    await message.reply_photo(
+        photo=url,
+        caption='Кот породы Сфинкс'
+    )
+
+
+@dp.message(Command('file'))
+async def handle_comand_pic(message: types.Message):
+    file_path = r"C:\Users\Тимур\Downloads\sphynx.jpeg"
+    await message.bot.send_chat_action(
+        chat_id=message.chat.id,
+        action=ChatAction.UPLOAD_DOCUMENT,
+    )
+    await message.reply_document(
+        document=types.FSInputFile(
+            path=file_path,
+            # filename="Cat_photo.jpeg"
+        ),
+        caption='Кот породы Сфинкс',
+    )
+
+
+@dp.message(Command("text"))
+async def send_txt_file(message: types.Message):
+    await message.bot.send_chat_action(
+        chat_id=message.chat.id,
+        action=ChatAction.UPLOAD_DOCUMENT,
+    )
+    file = io.StringIO()
+    file.write("Hello world!\n")
+    await message.reply_document(
+        document=types.BufferedInputFile(
+            file=file.getvalue().encode("utf-8"),
+            filename="file.txt"
+        ),
+    )
+
+
+async def sent_big_file(message: types.Message):
+    file = io.BytesIO()
+    url = "https://kotey-ka.ru/wp-content/uploads/2017/05/11111.jpg"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            result_bytes = await response.read()
+    file.write(result_bytes)
+    await message.reply_document(
+        document=types.BufferedInputFile(
+            # file=result_bytes,
+            file=file.getvalue(),
+            filename="Кот сфинкс, большая картинка"
+        ),
+    )
+
+@dp.message(Command("pic_file"))
+async def send_pic_file_buffered(message: types.Message):
+    await message.bot.send_chat_action(
+        chat_id=message.chat.id,
+        action=ChatAction.UPLOAD_DOCUMENT
+    )
+    # action_sender = ChatActionSender(
+    #     bot=message.bot,
+    #     chat_id=message.chat.id,
+    #     action=ChatAction.UPLOAD_DOCUMENT
+    # )
+    async with ChatActionSender.upload_document(
+            bot=message.bot,
+            chat_id=message.chat.id,
+    ):
+        await sent_big_file(message)
+
+
+
+
 @dp.message(F.photo, ~F.caption)
 async def handle_photo_wo_caption(message: types.Message):
-    await message.reply("Я не вижу, что на фото извините. Могли бы вы описать, что на нём изображено? 🙂")
+    caption = "Я не вижу, извини. Можешь подписать эту картинку?"
+    await message.reply_photo(
+        photo=message.photo[-1].file_id,
+        caption=caption,
+    )
+
+
+@dp.message(any_media_filter, ~F.caption)
+# @dp.message(F.photo, F.video, ~F.caption)
+# @dp.message(F.photo | F.video | F.document, ~F.caption)
+async def handle_any_media_wo_caption(message: types.Message):
+    if message.document:
+        await message.reply_document(
+            message.document.file_id
+        )
+    elif message.video:
+        await message.reply_video(
+            video=message.video.file_id,
+        )
+    else:
+        await message.reply("Я не вижу, извини. Не ругайся на меня.")
+
+
+@dp.message(F.photo, F.caption.contains("пожалуйста"))
+async def handle_photo_with_please_caption(message: types.Message):
+    await message.reply("Я не вижу, простите")
+
+
+@dp.message(any_media_filter, F.caption)
+async def handle_any_media_w_caption(message: types.Message):
+    await message.reply(f"Какой-то медиа файл. Ваш текст: {message.caption!r}")
+
+
+@dp.message(F.from_user.id.in_({42, 287612289}), F.text == "secret")
+async def secret_admin_message(message: types.Message):
+    await message.reply("Hi, admin!")
+
+
+# @dp.message(F.text.regexp(r"(\d+)", mode=RegexpMode.FINDALL).as_("code"))
+# async def handle_code(message: types.Message, code: list[str]):
+#     await message.reply(f"Your code: {code}")
+
+
+@dp.message(F.text.regexp(r"(\d+)", mode=RegexpMode.MATCH).as_("code"))
+async def handle_code(message: types.Message, code: Match[str]):
+    await message.reply(f"Your code: {code.group()}")
 
 
 @dp.message()
 async def echo_message(message: types.Message):
+    # await bot.send_message(
+    #     chat_id=message.chat.id,
+    #     text="Start processing...",
+    # )
+    # await bot.send_message(
+    #     chat_id=message.chat.id,
+    #     text="Detected message...",
+    #     reply_to_message_id=message.message_id,
+    # )
+
     await message.answer(
-        text='Подождите секунду, пожалуйста',
-        parse_mode=None
+        text="Подождите секунду, пожалуйста",
+        parse_mode=None,
     )
     # if message.text:
     #     await message.answer(
     #         text=message.text,
     #         entities=message.entities,
+    #         parse_mode=None,
     #     )
+    #     return
+    if message.sticker:
+        await message.bot.send_chat_action(
+            chat_id=message.chat.id,
+            action=ChatAction.CHOOSE_STICKER
+        )
     try:
         await message.copy_to(chat_id=message.chat.id)
         # await message.forward(chat_id=message.chat.id)
+        # await message.bot.forward_message(
+        #     chat_id
+        #     from_chat_id
+        #     message_id
+        # )
         # await message.send_copy(chat_id=message.chat.id)
     except TypeError:
-        await message.reply(text="Я не знаю что вам ответить, извините 🙂")
+        await message.reply(text="Я не знаю что вам ответить, извините")
 
 
 async def main():
     logging.basicConfig(level=logging.INFO)
     bot = Bot(
         token=settings.bot_token,
-        # parse_mode=ParseMode.MARKDOWN_V2
-        parse_mode=ParseMode.HTML
-        )
+        # parse_mode=ParseMode.MARKDOWN_V2,
+        parse_mode=ParseMode.HTML,
+    )
     await dp.start_polling(bot)
 
 
